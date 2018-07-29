288458c5f6de3821ab249d9be366b978de118c2e
Qasim Abbas <qasimabbas52@gmail.com>
2018-07-29 18:23:41 -0400

    Setting up the linter

--- one-app-ios/ViewController.swift
+++ one-app-ios/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
