1cb8f3be1151244607cec83585e81cce47507ddb
Qasim Abbas <qasimabbas52@gmail.com>
2018-07-29 18:43:00 -0400

    try again

--- install_swiftlint.sh
+++ install_swiftlint.sh
@@ -6,7 +6,8 @@
 
 set -e
 
-SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.9.1/SwiftLint.pkg"
+SWIFTLINT_PKG_PATH="/tmp/SwiftLint.pkg"
+SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.26.0/SwiftLint.pkg"
 
 wget --output-document=$SWIFTLINT_PKG_PATH $SWIFTLINT_PKG_URL
 
