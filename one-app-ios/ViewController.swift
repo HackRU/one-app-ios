aef12567b7f3b6f0b919813d6cdaa7a30c88b809
Qasim Abbas <qasimabbas52@gmail.com>
2018-07-29 17:58:50 -0400

    Adding project init files

---
+++ one-app-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  one-app-ios
+//
+//  Created by Qasim Abbas on 7/29/18.
+//  Copyright © 2018 HackRU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ one-app-iosTests/one_app_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  one_app_iosTests.swift
+//  one-app-iosTests
+//
+//  Created by Qasim Abbas on 7/29/18.
+//  Copyright © 2018 HackRU. All rights reserved.
+//
+
+import XCTest
+@testable import one_app_ios
+
+class one_app_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
