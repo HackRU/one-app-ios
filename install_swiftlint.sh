74065fb8761cd0992f7af892dc304e78fae22e10
Qasim Abbas <qasimabbas52@gmail.com>
2018-07-29 19:18:05 -0400

    Editing installer

--- install_swiftlint.sh
+++ install_swiftlint.sh
@@ -13,7 +13,7 @@ wget --output-document=$SWIFTLINT_PKG_PATH $SWIFTLINT_PKG_URL
 
 if [ -f $SWIFTLINT_PKG_PATH ]; then
   echo "SwiftLint package exists! Installing it..."
-  sudo installer -pkg $SWIFTLINT_PKG_PATH -target /
+  sudo install -pkg $SWIFTLINT_PKG_PATH -target /
 else
   echo "SwiftLint package doesn't exist. Compiling from source..." &&
   git clone https://github.com/realm/SwiftLint.git /tmp/SwiftLint &&
